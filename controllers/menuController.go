098e4c3d4ef1da24c783091289d534c0f4a8f491
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-09-09 02:50:51 -0700

    get menus function created

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,18 +1,37 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
+	"context"
+	// "fmt"
 	"github.com/samsorrahman/golang-rest-backend/database"
+	// "github.com/samsorrahman/golang-rest-backend/models"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+
+	// "go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	// "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var menuCollection *mongo.Collection = database.OpenCollection("menu")
 
 func GetMenus() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// c.JSON(200, gin.H{
-		// 	"message": "Get Menus",
-		// })
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := menuCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing the menu items"})
+		}
+		var allMenus []bson.M
+		if err = result.All(ctx, &allMenus); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allMenus)
 	}
 }
 
