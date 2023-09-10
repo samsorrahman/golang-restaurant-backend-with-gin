acd04b19f5bc79ff3f7ba0a4c19b4630c7141115
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-09-10 00:16:31 -0700

    GetInvoices function created

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -1,12 +1,34 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/samsorrahman/golang-rest-backend/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var invoiceCollection *mongo.Collection = database.OpenCollection("invoice")
 
 func GetInvoices() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// c.JSON(200, gin.H{
-		// 	"message": "Get Invoices",
-		// })
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		result, err := invoiceCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing invoice items"})
+		}
+
+		var allInvoices []bson.M
+		if err = result.All(ctx, &allInvoices); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allInvoices)
 	}
 }
 
