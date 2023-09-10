01e45f4dd49b5a1bd4b2efcf4917d61dfe80bd4d
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-09-10 02:12:12 -0700

    GetInvoice function created

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -8,9 +8,21 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/samsorrahman/golang-rest-backend/database"
+	"github.com/samsorrahman/golang-rest-backend/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
+
+type InvoiceViewFormat struct {
+	Invoice_id       string
+	Payment_method   string
+	Order_id         string
+	Payment_status   *string
+	Payment_due      interface{}
+	Table_number     interface{}
+	Payment_due_date time.Time
+	Order_details    interface{}
+}
 
 var invoiceCollection *mongo.Collection = database.OpenCollection("invoice")
 
@@ -34,17 +46,35 @@ func GetInvoices() gin.HandlerFunc {
 
 func GetInvoice() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// c.JSON(200, gin.H{
-		// 	"message": "Get Invoice",
-		// })
-	}
-}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		invoiceId := c.Param("invoice_id")
 
-func CreateInvoice() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// c.JSON(200, gin.H{
-		// 	"message": "Create Invoice",
-		// })
+		var invoice models.Invoice
+
+		err := invoiceCollection.FindOne(ctx, bson.M{"invoice_id": invoiceId}).Decode(&invoice)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing invoice item"})
+		}
+
+		var invoiceView InvoiceViewFormat
+
+		allOrderItems, err := ItemsByOrder(invoice.Order_id)
+		invoiceView.Order_id = invoice.Order_id
+		invoiceView.Payment_due_date = invoice.Payment_due_date
+
+		invoiceView.Payment_method = "null"
+		if invoice.Payment_method != nil {
+			invoiceView.Payment_method = *invoice.Payment_method
+		}
+
+		invoiceView.Invoice_id = invoice.Invoice_id
+		invoiceView.Payment_status = *&invoice.Payment_status
+		invoiceView.Payment_due = allOrderItems[0]["payment_due"]
+		invoiceView.Table_number = allOrderItems[0]["table_number"]
+		invoiceView.Order_details = allOrderItems[0]["order_items"]
+
+		c.JSON(http.StatusOK, invoiceView)
 	}
 }
 
